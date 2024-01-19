312c8ceb2946cdff0106451d9729993ed73b69b6
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 18:57:30 -0500

    Jan-19-2024-6-57-30-PM

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
-<template>
-  My App
+<template> 
+  <Header />
+  <div class="container">
+    <Balance />
+  </div>
 </template> 
+
+<script>
+  import Header from './components/Header.vue';
+  import Balance from './components/Balance.vue';
+
+  export default {
+  components: {
+    Header,
+    Balance,
+  },
+};
+</script>
