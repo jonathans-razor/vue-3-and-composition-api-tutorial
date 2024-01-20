0d6c27bf08af51c0090d610f6f0668894f5fad24
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 19:56:50 -0500

    Jan-19-2024-7-56-50-PM

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template> 
   <Header />
   <div class="container">
-    <Balance />
+    <Balance :total="total" />
     <IncomeExpenses />
     <TransactionList :transactions="transactions" />
     <AddTransaction />
@@ -15,13 +15,19 @@
   import TransactionList from './components/TransactionList.vue';
   import AddTransaction from './components/AddTransaction.vue';
 
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
 
 const transactions = ref([
   { id: 1, text: 'Flower', amount: -20 },
-  { id: 2, text: 'Salary', amount: 300 },
-  { id: 3, text: 'Book', amount: -10 },
+  { id: 2, text: 'Salary', amount: 800 },
+  { id: 3, text: 'Book', amount: -110 },
   { id: 4, text: 'Teriyaki', amount: -122 },
   { id: 5, text: 'Camera', amount: 150 },
 ]);
+
+//console.log(transactions.value);
+
+const total = computed(() => {
+  return transactions.value.reduce((acc, transaction) => acc += transaction.amount, 0);
+});
 </script>
