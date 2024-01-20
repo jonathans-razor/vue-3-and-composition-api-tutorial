4652f1756eef70bc2ad744bf03e06dfbd5524559
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 20:42:51 -0500

    Jan-19-2024-8-42-51-PM

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -18,10 +18,19 @@
 
 <script setup>  
   import { ref } from 'vue';
+  import { useToast } from "vue-toastification";
+
   const text = ref('');
   const amount = ref(0);
+  const toast = useToast();
 
   const onSubmit = () => {
+    if(text.value === '' || !amount.value) {
+      toast.error('Please add a text and an amount.');
+      return;
+    }
+    text.value = '';
+    amount.value = 0; 
     console.log('submit');
     console.log(text.value, amount.value);
   }
