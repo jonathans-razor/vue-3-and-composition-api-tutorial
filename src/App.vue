6bf44a021a26ad5b7dbedd75ceed19573ca51f2b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 19:01:44 -0500

    Jan-19-2024-7-01-43-PM

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <Header />
   <div class="container">
     <Balance />
+    <IncomeExpenses />
   </div>
 </template> 
 
 <script>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
 
   export default {
   components: {
     Header,
     Balance,
+    IncomeExpenses,
   },
 };
 </script>
