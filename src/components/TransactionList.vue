4e6385413bc6ccf122a3327b3818d2cbd7b082a4
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 19:31:09 -0500

    Jan-19-2024-7-31-06-PM

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,8 +1,8 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li v-for="transaction in transactions" :key="transaction.id" >
-      Cash <span>-$400</span><button class="delete-btn">x</button>
+    <li v-for="transaction in transactions" :key="transaction.id" :class="transaction.amount < 0 ? 'minus' : 'plus'" >
+      {{transaction.text}} <span>${{transaction.amount}}</span><button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
