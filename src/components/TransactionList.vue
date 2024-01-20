3857d8b9afdc975de7f445d6edf0288d3c37ed43
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2024-01-19 19:35:55 -0500

    Jan-19-2024-7-35-55-PM

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -7,17 +7,11 @@
   </ul>
 </template>
 
-<script>
-  export default {
-    data() {
-      return {
-        transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 },
-        ],
-      };
-    },
-  };
+<script setup>
+const transactions = [
+  { id: 1, text: 'Flower', amount: -20 },
+  { id: 2, text: 'Salary', amount: 300 },
+  { id: 3, text: 'Book', amount: -10 },
+  { id: 4, text: 'Camera', amount: 150 },
+];
 </script>
